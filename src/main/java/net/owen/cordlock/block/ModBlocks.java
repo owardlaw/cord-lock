267c4c9ef906b1ee1b5d171a2652889b9b003151
Owen Wardlaw <80442095+owardlaw@users.noreply.github.com>
2024-07-14 17:14:56 -0700

    added signal compass

---
+++ src/main/java/net/owen/cordlock/block/ModBlocks.java
@@ -0,0 +1,32 @@
+package net.owen.cordlock.block;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.owen.cordlock.CordLock;
+
+public class ModBlocks {
+//    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CordLock.MOD_ID);
+//
+//
+//    // Helper
+//    private static  <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+//        return BLOCKS.register(name, block);
+//    }
+//
+//    //    ts 3:43 vid #3
+//    // https://www.youtube.com/watch?v=d1fUtp_AAWY&list=PLKGarocXCE1HrC60yuTNTGRoZc6hf5Uvl&index=3
+//    //    TODO: setup custom items first
+//    //    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+//    //
+//    //        return ModItems
+//    //    }
+//
+//    public static void register(IEventBus eventBus) {
+//        BLOCKS.register(eventBus);
+//
+//    }
+}
