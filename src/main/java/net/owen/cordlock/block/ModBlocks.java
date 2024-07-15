d65af2e8bfffd0f3db1d004af30d4961bd65fa9e
Owen Wardlaw <80442095+owardlaw@users.noreply.github.com>
2024-07-14 23:18:43 -0700

    Code ready for custom block and added new asset pngs

--- src/main/java/net/owen/cordlock/block/ModBlocks.java
+++ src/main/java/net/owen/cordlock/block/ModBlocks.java
@@ -11,6 +11,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.owen.cordlock.CordLock;
+import net.owen.cordlock.block.custom.CoordinateCalibrator;
 import net.owen.cordlock.item.ModCreativeModeTab;
 import net.owen.cordlock.item.ModItems;
 
@@ -21,8 +22,9 @@ public class ModBlocks {
 
     // Add blocks here
     public static final RegistryObject<Block> COORDINATE_CALIBRATOR = registerBlock("coordinate_calibrator",
-            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
+            () -> new CoordinateCalibrator(BlockBehaviour.Properties.of(Material.WOOD)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.CORD_LOCK);
+
     // Helper
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
